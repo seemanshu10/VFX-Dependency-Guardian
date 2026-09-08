0b19044c2135ec2340ec5a698e9c4250f7e3e577
Seemanshu Verma <seemanshu0409@gmail.com>
2026-09-08 23:19:14 +0530

    main updated core version extraction logic added

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import os
 import json 
 
-
 project_file_dir = os.path.dirname(os.path.abspath(__file__))
-data = os.path.join(project_file_dir, r"data\packages\character_A\character_A.json")
+data_asset = os.path.join(project_file_dir, r"data\packages\character_A\character_A.json")
 
-with open(data, 'r') as file:
-    char_data = json.load(file)
+try :
+    with open(data_asset, 'r') as file:
+        char_data = json.load(file)
 
-# print(char_data)
+except FileNotFoundError:
+    print(f"Error :'{data_asset} doesn't exist'")
 
-# print(char_data["asset"])
-versions = char_data["components"]["rig"]["versions"]
-# print(versions.items())
+except json.JSONDecodeError as e:
+    print(f"Json decode error: {e} ")
 
-last_version_data = list(versions.values())[-1]
-print(last_version_data["status"])
+data_shot = os.path.join(project_file_dir, r"data\shots\SQ010_SH010.json")
 
-# for version, info in versions.items():
-#     print(version, info["status"])
+try :
+    with open(data_shot, 'r') as file:
+        char_shot = json.load(file)
+
+except FileNotFoundError:
+    print(f"Error :'{data_shot} doesn't exist'")
+
+except json.JSONDecodeError as e:
+    print(f"Json decode error: {e} ")
+
+def character_package_version(type):
+    versions = char_data["components"][type]["versions"]
+    last_version_status = list(versions.values())[-1]
+    last_version_version = list(versions.keys())[-1]
+    print(last_version_version, end= " ")
+    print(last_version_status["status"])
+
+def shot_type_version(asset_name, asset_type):
+    char_data = char_shot
+    # print(char_data)
+    versions = char_data["packages"][asset_name][asset_type]
+    print(versions)
+    
+if __name__ == "__main__":
+    character_package_version("rig")
+    shot_type_version("character_A", "rig")
+
+
